fdb117d1c4d13f52ffcdd0660da8bbb2a44ccabb
Segi Henggana <henggana7@gmail.com>
2017-09-02 17:52:11 +0700

    Add login page

---
+++ src/components/Login.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="measure-narrow center">
+      <h1 class="mt5">Login</h1>
+      <form 
+        action=""
+        class="">
+        <div class="mt3">
+          <label
+            for="login-username"
+            class="db fw6 lh-copy f6"
+          >
+            Username
+          </label>
+          <input
+            type="text"
+            class="pa2 input-reset ba bg-transparent w-100"
+            placeholder="username"
+          />
+        </div>
+        <div class="mt3">
+          <label
+            for="login-password"
+            class="db fw6 lh-copy f6"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            class="pa2 input-reset ba bg-transparent w-100"
+            placeholder="********"
+          />
+        </div>
+        <div class="mt3">
+          <input
+            class="b ph3 pv2 input-reset ba b--blue bg-transparent hover-bg-navy
+                   pointer f6 dib white bg-blue br1"
+            type="submit"
+            value="Sign in"
+          />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+};
+</script>
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="dt w-100 border-box pa3 ph3-ns bg-blue">
+    <router-link
+      to="/"
+      title="Home"
+      class="dtc v-mid white link dim w-25 pointer"
+    >
+      Vira
+    </router-link>
+    <div class="dtc v-mid w-75 tr">
+      <router-link 
+        to="/login"
+        title="About"
+        class="link dim white f6 f5-ns dib mr3 mr4-ns pointer"
+      >
+        Login
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      title: 'The Navbar yey!',
+    };
+  },
+};
+</script>
