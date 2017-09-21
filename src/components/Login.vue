a20b95fb30f5801bef80ebdca8bd58aa740ca43a
Segi Henggana <henggana7@gmail.com>
2017-09-21 17:51:39 +0700

    Add validation

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,51 +1,6 @@
-<template>
-  <div>
-    <div class="measure-narrow center">
-      <h1 class="mt5">Login</h1>
-      <form
-        @submit.prevent="submit">
-        <div class="mt3">
-          <label
-            for="login-username"
-            class="db fw6 lh-copy f6"
-          >
-            Username
-          </label>
-          <input
-            type="text"
-            class="pa2 input-reset ba bg-transparent w-100"
-            placeholder="username"
-          />
-        </div>
-        <div class="mt3">
-          <label
-            for="login-password"
-            class="db fw6 lh-copy f6"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            class="pa2 input-reset ba bg-transparent w-100"
-            placeholder="********"
-          />
-        </div>
-        <div class="mt3">
-          <input
-            class="b ph3 pv2 input-reset ba b--blue bg-transparent hover-bg-navy
-                   pointer f6 dib white bg-blue br1"
-            type="submit"
-            :value="submitText"
-            :disabled="isLoggingIn"
-          />
-        </div>
-      </form>
-    </div>
-  </div>
-</template>
-
 <script>
 import { mapState } from 'vuex';
+
 
 export default {
   name: 'login',
@@ -67,9 +22,77 @@ export default {
   },
   methods: {
     submit() {
-      this.$store.dispatch('login');
+      this.$validator.validateAll().then((valid) => {
+        if (!valid) return;
+        this.$store.dispatch('login');
+      });
     },
   },
 };
 </script>
+
+
+<template>
+  <div>
+    <div class="measure-narrow center">
+      <h1 class="mt5">Login</h1>
+      <form
+        @submit.prevent="submit">
+        <div class="mt3">
+          <label
+            for="login-username"
+            class="db fw6 lh-copy f6"
+          >
+            Username
+          </label>
+          <input
+            name="username"
+            type="text"
+            class="pa2 input-reset ba bg-transparent w-100"
+            placeholder="username"
+            v-validate="'required'"
+          />
+          <div
+            class="mt1 light-red"
+            v-show="errors.has('username')"
+          >
+            {{ errors.first('username')}}
+          </div>
+        </div>
+        <div class="mt3">
+          <label
+            for="login-password"
+            class="db fw6 lh-copy f6"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            class="pa2 input-reset ba bg-transparent w-100"
+            placeholder="********" 
+            v-validate="'required'"
+          />
+          <div
+            class="mt1 light-red"
+            v-show="errors.has('password')"  
+          >
+            {{ errors.first('password') }}
+          </div>
+        </div>
+        <div class="mt3">
+          <input
+            class="b ph3 pv2 input-reset ba b--blue bg-transparent hover-bg-navy
+                   pointer f6 dib white bg-blue br1"
+            type="submit"
+            :value="submitText"
+            :disabled="isLoggingIn"
+          />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VeeValidate from 'vee-validate';
 import App from './App';
 import store from './store';
 import router from './router';
 
+console.log('===== VEE VALIDATE', VeeValidate);
+Vue.use(VeeValidate);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
@@ -15,3 +18,4 @@ new Vue({
   template: '<App/>',
   components: { App },
 });
+
